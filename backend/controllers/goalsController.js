b8ca92e6760d812ed04e7f18515abc59a09367b9
Ndifreke Etukudo <ndifreke.etukudo@decagon.dev>
2022-12-29 16:41:25 +0100

    REST API for goals

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,9 +1,15 @@
 const asyncHandler = require ('express-async-handler')
+
+const Goal = require('../models/goalModel')
+
+
 //@desc  Getgoals
 //@route GET/api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message:'Get goals'})
+const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
    //@desc  Set goal 
 //@route POST/api/goals
@@ -13,7 +19,12 @@ const setGoal = asyncHandler(async(req, res) => {
     res.status(400)
     throw new Error('Please add a text feild')
    }
-    res.status(200).json({message:"Set goals"})
+
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
+        
+    res.status(200).json(goal)
 
 
     
@@ -22,15 +33,34 @@ const setGoal = asyncHandler(async(req, res) => {
 //@route PUT /api/goals/:id
 //@access Private
 const updateGoal = asyncHandler(async(req, res) => {
-    res.status(200).json({message:`Update goals${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal. findByIdAndUpdate(req.params.id, req.body.text,{
+        new: true,
+    } )
+
+
+    res.status(200).json(updatedGoal)
 })
 
 //@desc  Delete goals
 //@route DELETE /api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler(async(req, res) => {
+    const goal = await Goal.findById(req.params.id)
 
-    res.status(200).json({message:`Delete goals${req.params.id}`})
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    await Goal.findByIdAndRemove()
+
+    res.status(200).json({id: req.params.id}) 
 })
 
 
